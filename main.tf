a87432f42d31d3f992949e730ecfb1f3eb9b69f3
Aliaksandr Ushakou <aushakou@gmail.com>
2023-08-18 13:02:00 +0300

    Almost done

--- main.tf
+++ main.tf
@@ -10,3 +10,23 @@ terraform {
     dynamodb_table = "ushakou1"
   }
 }
+
+resource "null_resource" "update_kubeconfig" {
+  depends_on = [module.eks]
+  provisioner "local-exec" {
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  #name   = "ushakou-k8s"
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+    owner      = var.owner
+    GithubRepo = "terraform-aws-eks"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -4,27 +4,21 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "s3_bucket_name" {
-  description = "Name of S3 bucket for backend"
-  type        = string
-  default     = "ushakou-diplom"
-}
-
-variable "state_file_path" {
-  description = "Path to statefile"
-  type        = string
-  default     = "diplom1/state.tfstate"
-}
-
-variable "lock_table" {
-  description = "Name of tabel for locking"
-  type        = string
-  default     = "ushakou1"
-}
-
-
 variable "owner" {
   description = "Owner"
   type        = string
   default     = "ushakou"
 }
+
+variable "cluster_name" {
+  description = "Name of cluster"
+  type        = string
+  default     = "Ushakou-tf-eks"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR of VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
